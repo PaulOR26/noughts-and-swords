9df7e745bd8bac8739446c2771b3d298f998a964
PaulOR26 <pj_orourke@outlook.com>
2021-07-01 16:06:35 +0100

    work on button click for gameboard

--- noughtsAndCrosses/src/App.js
+++ noughtsAndCrosses/src/App.js
@@ -1,11 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-import GamePage from './components/GamePage';
+import Header from './components/Header';
+import Gameboard from './components/Gameboard';
+import TurnIndicator from './components/TurnIndicator';
+import PlayAgain from './components/PlayAgain';
+import { useState } from 'react';
 
 function App() {
+  const [turn, setTurn] = useState('noughts');
+  console.log(turn, 'app.js');
+
   return (
     <div className='App'>
-      <GamePage />
+      <Header />
+      <Gameboard turn={turn} />
+      <TurnIndicator />
+      <PlayAgain />
     </div>
   );
 }
--- noughtsAndCrosses/src/components/Gameboard.js
+++ noughtsAndCrosses/src/components/Gameboard.js
@@ -1,19 +1,98 @@
 import React from 'react';
+import { useState } from 'react';
 
-const Gameboard = () => {
+// function setGameboard(id, turn, cb) {
+//   if (turn === 'noughts') {
+//     setNoughts((currNoughts) => {
+//       const newString = currNoughts;
+//       return (newString += id);
+//     });
+//   }
+// }
+
+const Gameboard = ({ turn }) => {
+  console.log(turn, 'gameboard');
+  const [noughts, setNoughts] = useState('');
+  const [crosses, setCrosses] = useState('');
+
+  function buttonClick(id, turn) {
+    console.log(turn, 'onclick');
+    if (turn === 'noughts') {
+      setNoughts((currNoughts) => {
+        let newString = currNoughts;
+        console.log((newString, id));
+        return (newString += id);
+      });
+    }
+  }
+
   return (
     <div>
-      <button className='gameBoard'></button>
-      <button className='gameBoard'></button>
-      <button className='gameBoard'></button>
+      <button
+        onClick={() => {
+          buttonClick('1', turn);
+        }}
+        id='1'
+        className='gameBoard'
+      ></button>
+      {/* <button
+        onClick={() => {
+          setGameboard('2', turn);
+        }}
+        id='2'
+        className='gameBoard'
+      ></button>
+      <button
+        onClick={() => {
+          setGameboard('3', turn);
+        }}
+        id='3'
+        className='gameBoard'
+      ></button>
       <br></br>
-      <button className='gameBoard'></button>
-      <button className='gameBoard'></button>
-      <button className='gameBoard'></button>
+      <button
+        onClick={() => {
+          setGameboard('4', turn);
+        }}
+        id='4'
+        className='gameBoard'
+      ></button>
+      <button
+        onClick={() => {
+          setGameboard('5', turn);
+        }}
+        id='5'
+        className='gameBoard'
+      ></button>
+      <button
+        onClick={() => {
+          setGameboard('6', turn);
+        }}
+        id='6'
+        className='gameBoard'
+      ></button>
       <br></br>
-      <button className='gameBoard'></button>
-      <button className='gameBoard'></button>
-      <button className='gameBoard'></button>
+      <button
+        onClick={() => {
+          setGameboard('7', turn);
+        }}
+        id='7'
+        className='gameBoard'
+      ></button>
+      <button
+        onClick={() => {
+          setGameboard('8', turn);
+        }}
+        id='8'
+        className='gameBoard'
+      ></button>
+      <button
+        onClick={() => {
+          setGameboard('9', turn);
+        }}
+        id='9'
+        className='gameBoard'
+      ></button> */}
     </div>
   );
 };
