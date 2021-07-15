10f4511627aca87397294d69f0ea7cb311e918fc
nolixul <nadia.kaye@gmail.com>
2021-07-15 14:51:36 +0100

    netlify debugging

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
 const Header = () => {
-  return <h1>N⭕ughts and S⚔️ords</h1>;
+  return (
+    <h1>
+      N
+      <span role='img' aria-label='o'>
+        ⭕
+      </span>
+      ughts and S
+      <span role='img' aria-label='W'>
+        ⚔️
+      </span>
+      ords
+    </h1>
+  );
 };
 
 export default Header;
